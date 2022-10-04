a1d1a37e5c5bbacb0b324b27085a91c0ddd241d6
Noe <noe@gmail.com>
2022-10-04 18:30:02 -0500

    Agregar archivos de proyecto.

---
+++ Lab06/Lab06/ListViewGroup.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab06
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewGroup : ContentPage
+    {
+        ObservableCollection<group> personas = new ObservableCollection<group>();
+
+        public ObservableCollection<group> Personas { get { return personas; } }
+        public ListViewGroup()
+        {
+            InitializeComponent();
+            GroupView.ItemsSource = personas;
+            personas.Add(new group("Alpha", "A")
+            {
+                new Employee{DisplayName ="Alfie" },
+                new Employee{DisplayName ="Ava" },
+                new Employee{DisplayName ="Archie" },
+                new Employee{DisplayName ="Amelia" },
+            });
+            personas.Add(new group("Bravo", "B")
+            {
+                new Employee { DisplayName = "Brooke" },
+                new Employee { DisplayName = "Bobby" },
+                new Employee { DisplayName = "Bella" },
+                new Employee { DisplayName = "Ben" },
+            });
+            personas.Add(new group("Charlie", "C")
+            {
+                new Employee { DisplayName = "Carolina" },
+                new Employee { DisplayName = "Christian"},
+                new Employee { DisplayName = "Camila"},
+                new Employee { DisplayName = "Chachi"},
+            });
+            
+        }
+    }
+}
---
+++ Lab06/Lab06/group.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab06
+{
+    public class group: List<Employee>
+    {
+        public string Title { get; set; }
+        public string ShortName { get; set; }
+        public group(string title, string shortName)
+        {
+            Title = title;
+            ShortName = shortName;
+        }
+        public static IList<group> All { get; set; }
+    }
+}
